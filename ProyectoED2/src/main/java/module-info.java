8911ddb51dde016fdd488918ab6ebf5d933d9c3a
Nick Arévalo Ronquillo <69706202+Nick451109@users.noreply.github.com>
2022-06-30 19:46:58 -0500

    se agrego metodo para guardar imagenes a un paquete del proyecto

--- ProyectoED2/src/main/java/module-info.java
+++ ProyectoED2/src/main/java/module-info.java
@@ -1,7 +1,11 @@
 module com.mycompany.proyectoed2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.proyectoed2 to javafx.fxml;
     exports com.mycompany.proyectoed2;
+
+    opens com.mycompany.controllers to javafx.fxml;
+    exports com.mycompany.controllers;
 }
