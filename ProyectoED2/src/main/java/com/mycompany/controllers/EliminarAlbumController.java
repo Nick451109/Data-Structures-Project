8af076065418c5b7efa534526ccf0c9328fe9ad9
Carlos Loor <caraloor@espol.edu.ec>
2022-07-09 01:30:18 -0500

    avance
    avance

--- ProyectoED2/src/main/java/com/mycompany/controllers/EliminarAlbumController.java
+++ ProyectoED2/src/main/java/com/mycompany/controllers/EliminarAlbumController.java
@@ -4,9 +4,19 @@
  */
 package com.mycompany.controllers;
 
+import Datos.Alerta;
+import Datos.Fotografias;
+import Datos.Registro;
+import TDAs.ArrayList;
+import TDAs.DLinkedList;
+import com.mycompany.proyectoed2.App;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -15,12 +25,50 @@ import javafx.fxml.Initializable;
  */
 public class EliminarAlbumController implements Initializable {
 
+    @FXML
+    private ComboBox<String> comboAlb;
+    private ArrayList<String> albs;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        albs =Registro.getListaAlbumes();
+        for(int i = 0; i < albs.size(); i++){
+                comboAlb.getItems().add(albs.get(i));
+        }
+        
         // TODO
     }    
+
+    @FXML
+    private void EliminarAlb(MouseEvent event) throws IOException {
+        String albumSeleccionado = comboAlb.getValue();
+        DLinkedList<Fotografias> lFotografias = Registro.getListaFotos();
+        if (albumSeleccionado ==null){
+            Alerta.crearAlerta("Opcion Invalida", "No se ha seleccionado ningun album");
+        }else {
+            for( int i = lFotografias.size(); i >= 0; i-- ){
+                Fotografias foto= lFotografias.get(i);
+                if(albumSeleccionado.equals((foto.getAlbum()))){
+                   lFotografias.remove(i); 
+                }
+            }
+            Registro.setListaFotos(lFotografias);
+        }    
+        for(int i = 0; i < albs.size(); i++){
+            if( albs.get(i).equals(comboAlb.getValue())){
+                albs.remove(i);
+            }
+        }
+        Registro.setListaAlbumes(albs);
+        App.setRoot("VistaAlbumes");
+    }
+
+    @FXML
+    private void Regresar(MouseEvent event) throws IOException {
+        App.setRoot("VistaAlbumes");
+    }
     
 }
