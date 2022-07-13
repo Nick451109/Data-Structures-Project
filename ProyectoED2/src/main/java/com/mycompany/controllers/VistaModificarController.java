eb29dec179b5ad616cf28956a63f0dba394ba230
Carlos Loor <caraloor@espol.edu.ec>
2022-07-12 20:24:01 -0500

    avancesfinales
    avances

--- ProyectoED2/src/main/java/com/mycompany/controllers/VistaModificarController.java
+++ ProyectoED2/src/main/java/com/mycompany/controllers/VistaModificarController.java
@@ -14,6 +14,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -41,23 +42,39 @@ public class VistaModificarController implements Initializable { //agregar la la
     private Fotografias fotoAct;
     @FXML
     private Pane paneImg;
+    @FXML
+    private TextField LblLugar;
+    @FXML
+    private ComboBox<String> ListaAlb;
+    @FXML
+    private TextField LblDescripcion;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        ArrayList<String> albumes = Registro.getListaAlbumes();
+        for (int i = 0; i < albumes.size(); i++) {
+                System.out.println("entramos");
+                System.out.println(albumes.get(i));
+                ListaAlb.getItems().add(albumes.get(i));
+            }
         fotoAct = Registro.getFoto();
         paneImg.getChildren().clear();
+        ListaAlb.setValue(fotoAct.getAlbum());
         descripAlb.setText(fotoAct.getAlbum());
         decripLbl.setText(fotoAct.getDescripcion());
+        LblDescripcion.setText(fotoAct.getDescripcion()); // la descripcion 
         decripLgr.setText(fotoAct.getLugar());
+        LblLugar.setText(fotoAct.getLugar());  //obten en el lugar 
         personasCambio.setText(fotoAct.getPersonas().toString());
         descripFech.setText(fotoAct.getFecha().toString());
         final Image image = new Image("file:./src/main/resources/img/" + fotoAct.getiD() + ".jpg", 200, 200, true, false);
         ImageView imageView = new ImageView(image);
         imageView.setFitWidth(150);
         paneImg.getChildren().add(imageView);
+        System.out.println(ListaAlb.getValue()+"---------------------------");
     }    
 
     @FXML
@@ -72,7 +89,10 @@ public class VistaModificarController implements Initializable { //agregar la la
         for(String s: nueva){
             nuevaList.addLast(s);
         }
-        fotoAct.setPersonas(nuevaList);
+        fotoAct.setPersonas(nuevaList); // setea personas
+        fotoAct.setDescripcion(LblDescripcion.getText()); //setea la descripcion 
+        fotoAct.setLugar(LblLugar.getText()); // setea el lugar
+        fotoAct.setAlbum(ListaAlb.getValue()); // setea el album 
         Registro.setFoto(fotoAct);
         DLinkedList<Fotografias> actual = Registro.getListaFotos();
         Integer pos = actual.find(fotoAct);
